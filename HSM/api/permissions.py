cfc8f2fb0511a55ec0bfb01c9793f0a2a1b7f663
Sourabh Bera <70623039+SourabhBera@users.noreply.github.com>
2023-10-11 19:27:54 +0530

    Delete HSM directory

--- HSM/api/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-class IsPatient(permissions.BasePermission):
-    def has_permission(self, request, view): 
-        return request.user.groups.filter(name='Patients').exists()
-
-class IsDoctor(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.groups.filter(name='Doctors').exists()
