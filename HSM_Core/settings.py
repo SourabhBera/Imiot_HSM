6980f3eebd251afc8e7fa4d6283aeef0d9c35050
Sourabh Bera <70623039+SourabhBera@users.noreply.github.com>
2023-10-29 11:49:18 +0530

    Update settings.py

--- HSM_Core/settings.py
+++ HSM_Core/settings.py
@@ -149,3 +149,10 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_PORT = 587  
+EMAIL_USE_TLS = True  
+EMAIL_HOST_USER = 'blogggers.app@gmail.com'  
+EMAIL_HOST_PASSWORD = 'vhkx rcqi nfzv oalm'  
